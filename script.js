9babd7adcbb8e7cbfbf71febc617e00ef20ed03f
Jos <jejuarez97@gmail.com>
2023-06-08 22:14:00 -0700

    Refactor cellEle event listener so that callback is its own function and board data is updated; Added span element for future player data display

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// Game board data and player initialization
 const gameData = () => {
   const board = [
     "", "", "",
     "", "", "",
     "", "", "",
   ];
+
+  const winningSolutions = [];
   
   const getBoard = () => {
     return board;
@@ -17,25 +20,56 @@ const gameData = () => {
     board[index] = value;
   }
 
-  const playerOne = player('0');
-  const playerTwo = player('1');
+  const playerOne = player('Player 1', '0');
+  const playerTwo = player('Player 2', '1');
 
   return { getBoard, getCell, setCell, playerOne, playerTwo};
 }
 
-const player = (sign) => {
+// Player data
+const player = (name, sign) => {
   const playerSign = sign;
+  const playerName = name;
 
   const getPlayerSign = () => {
     return playerSign;
   }
 
-  return { getPlayerSign };
+  const getPlayerName = () => {
+    return playerName;
+  }
+
+  return { getPlayerName, getPlayerSign };
 }
 
-const renderGame = () => {
-  let player = 'zero';
+// Renders board to the DOM
+const renderGame = (game) => {
+  let player = game.playerOne.getPlayerSign();
+  const board = game.getBoard();
   const gameboard = document.getElementById("gameboard");
+
+  const checkWins = () => {
+    
+  }
+
+  // Fills gameboard cell according to player's turn
+  const playerMove = (e) => {
+    let index = e.target.id;
+    console.log("Cell " + index + " is filled");
+    const move = document.createElement('div');
+
+    if (player === '0') {
+      move.classList.add('zero');
+      board[Number(index)] = '0';
+      player = '1';
+    } else {
+      move.classList.add('one');
+      board[Number(index)] = '1';
+      player = '0';
+    }
+    e.target.appendChild(move);
+    console.log(board);
+  }
 
   for (let i = 0; i < 9; i++) {
     const cellEle = document.createElement('div');
@@ -43,34 +77,14 @@ const renderGame = () => {
     cellEle.setAttribute('id', i);
     gameboard.appendChild(cellEle);
 
-    cellEle.addEventListener('click', (e) => {
-      console.log(e.target.id);
-      const move = document.createElement('div');
-
-      if (player === 'zero') {
-        move.classList.add('zero');
-        player = 'one';
-      } else {
-        move.classList.add('one');
-        player = 'zero';
-      }
-      e.target.appendChild(move);
-    });
-  }
-
-  const playerMove = () => {
-
-  }
-  
-  const checkWins = () => {
-  
+    cellEle.addEventListener('click', playerMove, { once: true });
   }
 }
 
+// Initializes game data, renders board, starts game
 const gameController = (() => {
   const game = gameData();
-  console.log(game);
 
-  renderGame();
+  renderGame(game);
 
 })();
